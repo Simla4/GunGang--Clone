bd9684352509309b059b4f1f82bf5ca1378f150f
Simla Beken <38881186+Simla4@users.noreply.github.com>
2022-11-12 22:47:30 +0300

    Added: object pool script

--- GunGang--Clone/Assets/Scripts/Patterns/ObjectPooler.cs
+++ GunGang--Clone/Assets/Scripts/Patterns/ObjectPooler.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+
+    [SerializeField] private List<GameObject> activeList;
+    [SerializeField] private List<GameObject> inactiveList;
+
+    #endregion
+
+    #region Other Methods
+
+    public GameObject SpanwObject(GameObject obj, int poolSize)
     {
+        GameObject spawnedObj = null;
+
+        if (activeList.Contains(obj))
+        {
+            var index = inactiveList.BinarySearch(obj);
+            
+            spawnedObj = inactiveList[index];
+            inactiveList.RemoveAt(index);
+            
+            spawnedObj.SetActive(true);
+            
+            activeList.Add(spawnedObj);
+            return spawnedObj;
+        }
+        else
+        {
+            spawnedObj = Instantiate(obj);
+            
+            activeList.Add(spawnedObj);
+            
+            return spawnedObj;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveObject(GameObject obj)
     {
-        
+        inactiveList.Add(obj);
+        obj.SetActive(false);
+
+        activeList.Remove(obj);
     }
+
+    #endregion
 }
